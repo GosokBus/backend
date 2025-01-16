aa18dc17d4ca13ce45b5243c40133bfe001cf480
jay91537 <veuxft@gmail.com>
2025-01-17 05:10:10 +0900

    Feat : latest commit 5:10

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -23,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081") // 허용할 출처
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://35.216.46.102:8080/") // 허용할 출처
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
